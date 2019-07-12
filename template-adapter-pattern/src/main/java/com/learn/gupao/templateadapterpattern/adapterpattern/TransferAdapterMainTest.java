a146320d1580e62715ae0f0ba66a981eaef49f2d
MrJayden <38813450+MrJayden@users.noreply.github.com>
2019-07-12 20:36:32 +0800

    Add files via upload

--- template-adapter-pattern/src/main/java/com/learn/gupao/templateadapterpattern/adapterpattern/TransferAdapterMainTest.java
+++ template-adapter-pattern/src/main/java/com/learn/gupao/templateadapterpattern/adapterpattern/TransferAdapterMainTest.java
@@ -2,11 +2,26 @@ package com.learn.gupao.templateadapterpattern.adapterpattern;
 
 public class TransferAdapterMainTest {
     public static void main(String[] args) {
-        w2aTransfer();
+        w2wTransfer();
     }
 
     public static void w2aTransfer(){
-        PayUser wUser = new PayUser("W10001","A10001",234L);
+        PayUser wUser = new PayUser("W1001","A2001",234L);
+        wUser.doTrade();
+    }
+
+    public static void a2wTransfer(){
+        PayUser wUser = new PayUser("A2002","W1002",234L);
+        wUser.doTrade();
+    }
+
+    public static void a2aTransfer(){
+        PayUser wUser = new PayUser("A2001","A2002",234L);
+        wUser.doTrade();
+    }
+
+    public static void w2wTransfer(){
+        PayUser wUser = new PayUser("W1001","W1002",234L);
         wUser.doTrade();
     }
 }
